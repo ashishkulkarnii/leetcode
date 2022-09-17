997e9de67e7ba94be055d3d29bf73e8f4fc8f1e1
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-09-17 16:08:16 +0530

    Time: 88 ms (11.17%), Space: 13.5 MB (91.78%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+class Solution {
+public:
+    int atoi(char c) {
+        return (int) c - 48;
+    }
+    int minPartitions(string n) {
+        int res = 0;
+        for(int i = 0; i < n.length(); i++){
+            if(atoi(n[i]) > res) {
+                res = atoi(n[i]);
+            }
+        }
+        return res;
+    }
+};
