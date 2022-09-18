f8391719e31e1b56ef55554201ef02cd9d6c7874
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-09-18 18:40:43 +0530

    Time: 23 ms (5.42%), Space: 8.7 MB (78.82%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -26,16 +26,3 @@ public:
         return min;
     }
 };
-
-// def minimumTotal(self, triangle: List[List[int]]) -> int:
-//         for i in range(1, len(triangle)):
-//             for j in range(len(triangle[i])):
-//                 if j == 0:
-//                     triangle[i][j] += triangle[i-1][0]
-//                 elif j == len(triangle[i]) - 1:
-//                     triangle[i][j] += triangle[i-1][len(triangle[i-1]) - 1]
-//                 else:
-//                     t1 = triangle[i-1][j-1]
-//                     t2 = triangle[i-1][j]
-//                     triangle[i][j] += min([t1,t2])
-//         return(min(triangle[-1]))
