6e726459980ff70c8ea820c11ff07c9dd9ccd300
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-10-04 14:19:07 +0530

    Time: 4393 ms (5.48%), Space: 21.4 MB (53.66%) - LeetHub

---
+++ 1558-minimum-numbers-of-function-calls-to-make-target-array/1558-minimum-numbers-of-function-calls-to-make-target-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def allEven(self, arr):
+        for i in arr:
+            if i % 2 == 1:
+                return False
+        return True
+    def by2(self, arr):
+        for i in range(len(arr)):
+            arr[i] = int(arr[i] / 2)
+        return arr
+    def minOperations(self, nums: List[int]) -> int:
+        op = 0
+        while nums.count(0) != len(nums):
+            for i in range(len(nums)):
+                if nums[i] % 2 == 1:
+                    nums[i] -= 1
+                    op += 1
+            if nums.count(0) != len(nums):
+                nums = self.by2(nums)
+                op += 1
+        return op
