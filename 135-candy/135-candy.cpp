3d71e4eb3b4c19e52d0fe0d0035d7ca844ad141b
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-10-08 16:15:55 +0530

    Time: 57 ms (24.82%), Space: 17.8 MB (39.88%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
+        
         if(n == 1) {
             return 1;
         }
+        
         vector<int> candies(n, 0);
+        
         for(int i = 1; i < n; ++i) {
             if(ratings[i-1] < ratings[i]) {
                 candies[i] += candies[i-1] + 1;
             }
         }
+        
         for(int i = n - 2; i >= 0; --i) {
             if(ratings[i+1] < ratings[i]) {
                 if(candies[i] <= candies[i+1]) {
@@ -18,7 +22,9 @@ public:
                 }
             }
         }
+        
         int offset = 1 - *min_element(candies.begin(), candies.end());
+        
         return accumulate(candies.begin(), candies.end(), offset * n);
     }
 };
