3ce8d5f9482cde6accd08a9af4ecb3c6b2b4b3ca
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-10-10 16:48:13 +0530

    Time: 32 ms (92.92%), Space: 13.8 MB (61.62%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -8,15 +8,9 @@ class Solution:
             if palindrome[c] > 'a':
                 new[c] = 'a'
                 break
-            else:
-                pass
         if palindrome == new:
             for c in range(int(len(palindrome))-1, -1, -1):
                 if palindrome[c] < 'z':
                     new[c] = chr(ord(palindrome[c]) + 1)
                     break
-                else:
-                    pass
-        if new == palindrome:
-            return ''
         return ''.join(new)
