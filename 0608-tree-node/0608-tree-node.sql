ab1ad488d2301df642447fc895f0b89e5fe231ae
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-10-13 18:35:26 +0530

    Time: 624 ms (34.47%), Space: 0B (100.00%) - LeetHub

---
+++ 0608-tree-node/0608-tree-node.sql
@@ -0,0 +1,5 @@
+(select id, 'Root' as type from tree where p_id is null)
+union
+(select id, 'Leaf' as type from tree where id not in (select p_id from tree where p_id is not null) and p_id is not null)
+union
+(select id, 'Inner' as type from tree where id in (select p_id from tree where p_id is not null) and p_id is not null)
