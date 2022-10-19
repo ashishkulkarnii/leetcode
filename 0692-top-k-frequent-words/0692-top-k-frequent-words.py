c7c21d2f8fb85e83c153a2ad28facb88f2d5c247
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-10-19 09:34:35 +0530

    Time: 127 ms (30.27%), Space: 14 MB (64.49%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        unique_words = sorted(list(set(words)))
+        unique_words = list(set(words))
+        unique_words.sort()
         unique_words.sort(key = lambda x: words.count(x), reverse = True)
-        return unique_words[0:k:1]
+        return unique_words[:k:]
