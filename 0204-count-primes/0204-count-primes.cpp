bf0267d21ed2ae15eadfe0bc17d343810d3cf8aa
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-10-20 13:57:25 +0530

    Time: 428 ms (26.16%), Space: 11.5 MB (17.57%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n == 0 or n == 1) {
+            return 0;
+        }
+        bool prime[n];
+        for(auto &e: prime) {
+            e = true;
+        }
+        for(int i = 2; i < n; i++) {
+            if(prime[i] == false) {
+                continue;
+            }
+            else {
+                for(int j = 2 * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        return count(prime + 2, prime + n, true);
+    }
+};
