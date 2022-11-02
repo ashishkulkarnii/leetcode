2f9449f7cc830c613291c481ca936608fb5869b6
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-11-02 12:07:52 +0530

    Time: 264 ms (96.60%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,14 @@
+select
+    case
+        when id = 
+            (
+                select max(s.id)
+                from seat as s
+            ) 
+            and id % 2 = 1 then id
+        when id % 2 = 0 then id - 1
+        when id % 2 = 1 then id + 1
+    end as id,
+    student
+from seat
+order by id asc
