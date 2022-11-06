7b2bb62c49e78c5454c3250885f65aff2e18335d
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-11-06 21:51:19 +0530

    Time: 35 ms (97.62%), Space: 15.1 MB (7.14%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.py
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -1,9 +1,3 @@
 class Solution:
     def orderlyQueue(self, s: str, k: int) -> str:
-        if k > 1:
-            return ''.join(sorted(s))
-        res = s
-        for i, c in enumerate(s):
-            if res > s[i::] + s[:i:]:
-                res = s[i::] + s[:i:]
-        return res
+        return min([s[i::] + s[:i:] for i in range(len(s))]) if k == 1 else ''.join(sorted(s))
