6bbaef6d51e750674bfe6dc4594703a1be1937e8
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-11-08 08:44:24 +0530

    Time: 90 ms (9.41%), Space: 13.9 MB (15.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -4,6 +4,6 @@ class Solution:
         while i <= len(s) - 2:
             if abs(ord(s[i]) - ord(s[i+1])) == ord('a') - ord('A'):
                 s = s[:i:] + s[i+2::]
-                i = -1
+                i = max(-1, i-2)
             i += 1
         return s
