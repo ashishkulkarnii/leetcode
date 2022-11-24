1c2a755101877534b2eb321b23f0e77910fe6320
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-11-24 20:45:00 +0530

    Time: 372 ms (82.28%), Space: 8.1 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -23,10 +23,8 @@ public:
         m = board[0].size();
         for(int i = 0; i < n; ++i) {
             for(int j = 0; j < m; ++j) {
-                if(board[i][j] == word[0]) {
-                    if(dfs(i, j, 0)) {
-                        return true;
-                    }
+                if(dfs(i, j, 0)) {
+                    return true;
                 }
             }
         }
