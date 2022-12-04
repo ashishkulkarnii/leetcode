0d80c198a8dc22454796c665a2864369100d684f
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-12-04 12:32:23 +0530

    Time: 3382 ms (7.22%), Space: 33.2 MB (5.46%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumAverageDifference(self, nums: List[int]) -> int:
+        prefix = copy.deepcopy(nums)
+        suffix = copy.deepcopy(nums)
+        n = len(nums)
+        for i in range(1, n):
+            prefix[i] += prefix[i-1]
+            suffix[n-i-1] += suffix[n-i]
+        res = sum(nums)
+        min_index = -1
+        for i in range(n):
+            suffix[i] -= nums[i]
+            if n - 1 - i > 0:
+                if abs(prefix[i]//(i+1) - suffix[i]//(n-1-i)) < res:
+                    res = abs(prefix[i]//(i+1) - suffix[i]//(n-1-i))
+                    min_index = i
+            else:
+                if prefix[i]//(i+1) < res:
+                    res = prefix[i]//(i+1)
+                    min_index = i
+        if min_index == -1:
+            return 0
+        return min_index
