4127c5dd9f9c2fb9e6ad708689bc7fe399fe0acc
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-12-07 10:34:10 +0530

    Time: 634 ms (79.45%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,9 @@
+select employee_id,
+    (
+        case
+            when employee_id mod 2 = 1 and name not like 'M%' then salary
+            else 0
+        end
+    ) as bonus
+from employees
+order by employee_id;
