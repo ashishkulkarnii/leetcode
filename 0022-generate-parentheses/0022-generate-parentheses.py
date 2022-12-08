8ae6f855760fba4c44665e2a83e25fe56ede77b5
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-12-09 00:45:43 +0530

    Time: 979 ms (5.05%), Space: 191 MB (6.64%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,10 +2,10 @@ class Solution:
     def gen(self, s, n, res):
         if n > 0:
             for i in range(len(s)+1):
-                self.gen(s[:i]+'()'+s[i:], n-2, res)
+                self.gen(s[:i]+'()'+s[i:], n-1, res)
         else:
             res.append(s)
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        self.gen('', n*2, res)
+        self.gen('', n, res)
         return list(set(res))
