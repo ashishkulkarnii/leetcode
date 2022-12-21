7400b969b9ef8503e4589a46079bc90a86373814
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-12-21 14:32:20 +0530

    Time: 2430 ms (7.64%), Space: 20 MB (92.70%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,21 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        if not dislikes:
+            return True
+        adj_list = [[] for _ in range(n)]
+        for a, b in dislikes:
+            adj_list[a-1].append(b-1) 
+            adj_list[b-1].append(a-1)
+        vertices = sorted(list(range(n)), key=lambda x: len(adj_list[x]), reverse=True)
+        colors = [0] * n
+        colors[vertices[0]] = 1
+        for v in vertices:
+            if colors[v] == 0:
+                continue
+            neighbors = adj_list[v]
+            for n in neighbors:
+                if colors[n] == colors[v]:
+                    return False
+                else:
+                    colors[n] = -colors[v]
+        return True
