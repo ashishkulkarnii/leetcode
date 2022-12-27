d13201b09dacf1edc5b0ca9ea5af79014efb1b0b
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2022-12-27 10:23:54 +0530

    Time: 968 ms (86.84%), Space: 22.2 MB (49.34%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.py
@@ -4,10 +4,7 @@ class Solution:
         unused_space = sorted([capacity[i] - rocks[i] for i in range(n)])
         ans = 0
         for i in range(n):
-            if unused_space[i] == 0:
-                ans += 1
-            elif unused_space[i] <= additionalRocks:
+            if unused_space[i] <= additionalRocks:
                 ans += 1
                 additionalRocks -= unused_space[i]
         return ans
-            
