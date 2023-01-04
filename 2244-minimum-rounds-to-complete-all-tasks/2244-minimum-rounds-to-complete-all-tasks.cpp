842a918412699825d0046272add127d986306422
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-01-04 12:34:07 +0530

    Time: 562 ms (32.44%), Space: 103.5 MB (89.36%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> t;
+        for(auto x: tasks) {
+            if(t.find(x) == t.end())
+                t[x] = 1;
+            else
+                t[x] += 1;
+        }
+        int res = 0;
+        for(auto x: t) {
+            if(x.second == 1)
+                return -1;
+            else
+                res += x.second / 3 + (x.second - 3 * floor(x.second / 3) == 1 || x.second - 3 * floor(x.second / 3) == 2);
+        }
+        return res;
+    }
+};
