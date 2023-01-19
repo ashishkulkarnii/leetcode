3736206d0d2b15d09d0ed9b667c6ddfca689715c
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-01-19 11:05:34 +0530

    Time: 47 ms (44.89%), Space: 14 MB (32.58%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        earth = 'abcdefghijklmnopqrstuvwxyz'
+        earth_to_alien = dict()
+        alien_to_earth = dict()
+        for i in range(26):
+            alien_to_earth[order[i]] = earth[i]
+        for i in range(len(words)):
+            word = list(words[i])
+            for j in range(len(word)):
+                word[j] = alien_to_earth[word[j]]
+            words[i] = ''.join(word)
+        if words == sorted(words):
+            return True
+        return False
