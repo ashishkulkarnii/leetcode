de7725245604157472ff541be6c948691bbbb6d9
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-02-01 11:50:46 +0530

    Time: 38 ms (61.33%), Space: 13.8 MB (70.81%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,7 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         a, b = (str1, str2) if len(str1) >= len(str2) else (str2, str1)
-        res = ''
+        res = ""
         for i in range(1, len(b) + 1):
             if int(len(a) / i) * b[:i:] == a and int(len(b) / i) * b[:i:] == b:
                 res = b[:i:]
