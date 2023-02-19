d1153213e314756ce66909ce7b04d594e446bd57
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-02-19 16:39:35 +0530

    Time: 134 ms (26.62%), Space: 31.7 MB (5.44%) - LeetHub

--- 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
+++ 0807-max-increase-to-keep-city-skyline/0807-max-increase-to-keep-city-skyline.py
@@ -3,18 +3,16 @@ import numpy as np
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
         N = len(grid)
-        
+        res = 0
+
         grid = np.array(grid)
         ns = np.array([0] * N)
         ew = np.array([0] * N)
-        maxes = np.zeros(shape=(N, N))
-        
-        res = 0
-        
+
         for i in range(N):
             ns[i] = max(grid[:, i])
             ew[i] = max(grid[i, :])
-        
+
         for x in range(N):
             for y in range(N):
                 curr_max = min(ns[x], ew[y])
