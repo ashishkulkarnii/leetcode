e91293fea0b5c065485ef560c292889ebf0f6495
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-02-21 09:57:42 +0530

    Time: 1 ms (100.00%), Space: 50.3 MB (85.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,15 +1,19 @@
-// O(n logn) time
-
 class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int res = 1;
-        for(auto n: nums) {
-            if(res == n) {
-                res += 1;
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length, temp;
+        for(int i = 0; i < n; ++i) {
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
+                temp = nums[i] - 1;
+                nums[i] = nums[i] ^ nums[temp];
+                nums[temp] = nums[i] ^ nums[temp];
+                nums[i] = nums[i] ^ nums[temp];
             }
         }
-        return res;
+        for(int i = 0; i < n; ++i) {
+            if(nums[i] - 1 != i) {
+                return i + 1;
+            }
+        }
+        return n + 1;
     }
-};
+}
