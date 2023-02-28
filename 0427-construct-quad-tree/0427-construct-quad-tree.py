9725e41b8ab7b3eff5408da60d5c39598ee70237
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-02-28 10:15:56 +0530

    Time: 283 ms (5.82%), Space: 32.4 MB (5.26%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.py
+++ 0427-construct-quad-tree/0427-construct-quad-tree.py
@@ -15,18 +15,7 @@ class Solution:
     def buildTree(self, grid):
         if len(grid) ** 2 == np.count_nonzero(grid == 1) or len(grid) ** 2 == np.count_nonzero(grid == 0):
             return Node(grid[0][0], True, None, None, None, None)
-        q1 = grid[:len(grid)//2, len(grid)//2:]
-        q2 = grid[:len(grid)//2, :len(grid)//2]
-        q3 = grid[len(grid)//2:, :len(grid)//2]
-        q4 = grid[len(grid)//2:, len(grid)//2:]
-        tr = self.buildTree(q1)
-        tl = self.buildTree(q2)
-        bl = self.buildTree(q3)
-        br = self.buildTree(q4)
-        return Node(False, False, tl, tr, bl, br)
+        return Node(False, False, self.buildTree(grid[:len(grid)//2, :len(grid)//2]), self.buildTree(grid[:len(grid)//2, len(grid)//2:]), self.buildTree(grid[len(grid)//2:, :len(grid)//2]), self.buildTree(grid[len(grid)//2:, len(grid)//2:]))
     
     def construct(self, grid: List[List[int]]) -> 'Node':
-        if not grid:
-            return None
-        grid = np.array(grid)
-        return self.buildTree(grid)
+        return None if not grid else self.buildTree(np.array(grid))
