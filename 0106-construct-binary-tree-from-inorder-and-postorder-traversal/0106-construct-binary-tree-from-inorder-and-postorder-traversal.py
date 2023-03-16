f0777dff080e4b6aca6e89dfd7873ee0fd56ad33
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-03-16 20:50:24 +0530

    Time: 197 ms (33.61%), Space: 89 MB (12.74%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -6,20 +6,15 @@
 #         self.right = right
 class Solution:
     def buildTree(self, inorder: List[int], postorder):
-        root = TreeNode(postorder[-1])
-        self.buildTree1(inorder, postorder, root)
+        self.buildTree1(inorder, postorder, root:=TreeNode(postorder[-1]))
         return root
     def buildTree1(self, inorder: List[int], postorder: List[int], node):
         if not node:
             return
         node_pos = inorder.index(node.val)
-        left_subtree_inorder = inorder[:node_pos]
-        left_subtree_postorder = postorder[:len(left_subtree_inorder)]
-        right_subtree_inorder = inorder[node_pos+1:]
-        right_subtree_postorder = postorder[len(left_subtree_inorder):-1]
-        if left_subtree_inorder:
-            node.left = TreeNode(left_subtree_postorder[-1])
+        if left_subtree_inorder := inorder[:node_pos]:
+            node.left = TreeNode((left_subtree_postorder:=postorder[:len(left_subtree_inorder)])[-1])
             self.buildTree1(left_subtree_inorder, left_subtree_postorder, node.left)
-        if right_subtree_inorder:
-            node.right = TreeNode(right_subtree_postorder[-1])
+        if right_subtree_inorder := inorder[node_pos+1:]:
+            node.right = TreeNode((right_subtree_postorder:=postorder[len(left_subtree_inorder):-1])[-1])
             self.buildTree1(right_subtree_inorder, right_subtree_postorder, node.right)
