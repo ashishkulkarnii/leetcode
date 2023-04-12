bc4caf07ad703887f84bc0d276b6dec71081b485
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-04-12 16:16:13 +0530

    Time: 475 ms (100.00%), Space: 74.8 MB (100.00%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const memos = {};
+    return function(...args) {
+        if(!(args in memos)) {
+            memos[args] = fn(...args);
+        }
+        return memos[args];
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
