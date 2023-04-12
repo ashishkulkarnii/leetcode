f71da1bfe94c2c3169c135793e42d8e44c81deed
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-04-12 16:07:10 +0530

    Time: 75 ms (100.00%), Space: 43.8 MB (100.00%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return (x) => {
+        var ans = x;
+        for(let i = functions.length - 1; i >= 0; --i) {
+            ans = functions[i](ans);
+        }
+        return ans;
+    };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
