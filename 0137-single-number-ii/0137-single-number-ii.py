59a5a40f77a2f557a1ec986de74eab8ac4b3f554
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-07-04 17:01:53 +0530

    Time: 77 ms (56.91%), Space: 18.5 MB (20.63%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,11 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hm = {}
-        for n in nums:
-            if n in hm.keys():
-                hm[n] += 1
-            else:
-                hm[n] = 1
-        for n, c in hm.items():
-            if c == 1:
-                return n
+        for key, value in collections.Counter(nums).items():
+            if value == 1:
+                return key
