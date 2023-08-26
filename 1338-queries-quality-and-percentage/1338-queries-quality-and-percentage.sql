13ecbc810bdeedcce2a54ad68b260bee4b8e17ca
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2023-08-26 12:10:08 +0530

    Time: 1531 ms (34.69%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,14 @@
+select query_name,
+    (
+        round(avg(rating / position), 2)
+    ) as quality,
+    (
+        round(sum(
+            case
+                when rating < 3 then 1
+                else 0
+            end
+        ) * 100 / count(*), 2)
+    ) as poor_query_percentage
+from Queries
+group by query_name;
