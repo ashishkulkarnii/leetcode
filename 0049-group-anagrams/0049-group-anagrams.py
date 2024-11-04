c308691681fe6f5b70623964bfec38dd4598a506
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2024-11-04 21:46:28 +0530

    Time: 11 ms (80.18%), Space: 19.5 MB (87.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,10 @@
 class Solution:
-    def same(self, s1, s2):
-        return sorted(s1) == sorted(s2)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hmap = dict()
-        for word in strs:
-            sw = ''.join(sorted(list(word)))
-            if sw not in hmap.keys():
-                hmap[sw] = [word]
+        hm = dict()
+        for s in strs:
+            ss = "".join(sorted(list(s)))
+            if ss in hm.keys():
+                hm[ss].append(s)
             else:
-                hmap[sw].append(word)
-        return hmap.values()
+                hm[ss] = [s]
+        return list(hm.values())
