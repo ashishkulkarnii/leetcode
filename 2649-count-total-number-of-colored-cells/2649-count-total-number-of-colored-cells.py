ef8b49d89185a7342fd25585dfb1bc977b726199
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2025-03-05 14:24:04 +0530

    Time: 0 ms (100%), Space: 17.9 MB (30.45%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -1,6 +1,25 @@
+"""minute-by-minute:
+1. 1
+2. 5 = 1 + 4
+3. 13 = 5 + 8
+4. 25 = 13 + 12
+5. 41 = 25 + 16
+
+we see that at nth minute, we are adding 4(n-1) blocks to the number of blocks at the (n-1)th minute.
+
+```
+blue_count(n) = blue_count(n-1) + 4 * (n - 1)
+bc(0) = 0
+bc(1) = 1
+bc(2) = bc(1) + 4(2-1)
+bc(3) = bc(2) + 4(3-1)
+bc(4) = bc(3) + 4(4-1)
+      = bc(2) + 4(3-1) + 4(4-1)
+      = bc(1) + 4(2-1) + 4(3-1) + 4(4-1)
+      = bc(1) + 4(1 + 2 + 3)
+bc(n) = 1 + 4 * n * (n - 1) / 2
+"""
+
 class Solution:
     def coloredCells(self, n: int) -> int:
-        res = 1
-        for i in range(2, n + 1):
-            res += (i - 1) * 4
-        return res
+        return 1 + 2 * n * (n - 1)
