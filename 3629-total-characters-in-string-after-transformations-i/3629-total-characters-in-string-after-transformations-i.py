d9df016641ab230fd274297856dbf899b8da7aad
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2025-05-14 11:43:55 +0530

    Time: 5538 ms (19.47%), Space: 19.4 MB (24.16%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
@@ -1,23 +1,20 @@
 class Solution:
-    def lengthAfterTransformations(self, s: str, t: int) -> int:
-        def inx(c):
-            return ord(c) - ord('a')
-        
-        res = len(s)
-        mod = 10 ** 9 + 7
+    def lengthAfterTransformations(self, s: str, t: int) -> int:        
+        mod = 1000000007
         count = [0] * 26
+        zero = ord('a')
 
         for c in list(s):
-            count[inx(c)] += 1
+            count[ord(c) - zero] += 1
         
         for _ in range(t):
             tmp = [0] * 26
             for i, cnt in enumerate(count):
                 if i < 25:
-                    tmp[i+1] += count[i]
+                    tmp[i+1] += cnt
                 else:
-                    tmp[0] += count[i]
-                    tmp[1] += count[i]
+                    tmp[0] += cnt
+                    tmp[1] += cnt
             count = tmp
         
         return sum(count) % mod
