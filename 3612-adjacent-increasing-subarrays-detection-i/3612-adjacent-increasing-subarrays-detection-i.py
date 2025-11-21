76a142e04bd7ba21ac6b9d593f0b973a7324a837
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2025-11-20 17:58:14 -0800

    Time: 80 ms (80.27%), Space: 17.8 MB (57.51%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
@@ -3,12 +3,10 @@ class Solution:
         s = 0
         salen = [0 for _ in nums]
         for i in range(1, len(nums)):
-            print(salen)
             if nums[i] <= nums[i-1]:
                 salen[s] = i - s
                 s = i
         salen[s] = len(nums) - s
-        print(salen)
         for i, l in enumerate(salen):
             if l >= k * 2:
                 return True
