7f46db0348e358a0deca55319fa6a8bd6821e8c4
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-01-05 13:13:37 -0800

    Time: 0 ms (100%), Space: 17.3 MB (99.29%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -2,8 +2,5 @@ class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
         i = 0
         while i < len(bits) - 1:
-            if bits[i] == 1 and bits[i+1] in (0, 1):
-                i += 2
-            else:
-                i += 1
+            i += bits[i] + 1
         return bool(len(bits) - i)
