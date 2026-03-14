f9d29477c5a4035f9fd108a1fb3521cb91e6d6cf
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-03-14 16:22:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,23 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        window_freq = collections.Counter(s[0])
-        target_freq = collections.Counter(t)
-        def check():
-            for char, freq in target_freq.items():
-                if freq > window_freq[char]:
+        counter = collections.defaultdict(int)
+        t_counter = collections.Counter(t)
+        i, j = 0, 0
+        def check_counter(counter):
+            for c in t:
+                if counter[c] < t_counter[c]:
                     return False
             return True
-        i, j = 0, 0
-        res = ""
-        res_len = len(s) + 1
-        while i <= j < len(s):
-            if check():
-                if j + 1 - i < res_len:
-                    res = s[i:j+1]
-                    res_len = j + 1 - i
-                window_freq[s[i]] -= 1
-                i += 1
-            else:
-                j += 1
-                if j < len(s): window_freq[s[j]] += 1
-        return res
+        res = s
+        while j < len(s):
+            counter[s[j]] += 1
+            if check_counter(counter):
+                while check_counter(counter):
+                    counter[s[i]] -= 1
+                    i += 1
+                res = min(res, s[i-1:j+1], key=len)
+            j += 1
+        return res if (res != s or check_counter(collections.Counter(res))) else ""
