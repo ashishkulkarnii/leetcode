4274ae44ee9c1bbfe679eb8577d460bf9a17bbce
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-03-14 16:24:29 -0700

    Time: 535 ms (7.88%), Space: 19.7 MB (67.36%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,24 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        counter = collections.defaultdict(int)
-        t_counter = collections.Counter(t)
-        i, j = 0, 0
-        def check_counter(counter):
-            for c in t:
-                if counter[c] < t_counter[c]:
+        window_freq = collections.Counter(s[0])
+        target_freq = collections.Counter(t)
+        def check():
+            for char, freq in target_freq.items():
+                if freq > window_freq[char]:
                     return False
             return True
+        i, j = 0, 0
         res = ""
         res_len = len(s) + 1
-        while j < len(s):
-            counter[s[j]] += 1
-            if check_counter(counter):
-                while check_counter(counter):
-                    counter[s[i]] -= 1
-                    i += 1
-                if j - i + 1 <= res_len:
-                    res = s[i-1:j+1]
-                    res_len = j - i + 1
-                # res = min(res, s[i-1:j+1], key=len)
-            j += 1
+        while i <= j < len(s):
+            if check():
+                if j + 1 - i < res_len:
+                    res = s[i:j+1]
+                    res_len = j + 1 - i
+                window_freq[s[i]] -= 1
+                i += 1
+            else:
+                j += 1
+                if j < len(s): window_freq[s[j]] += 1
         return res
