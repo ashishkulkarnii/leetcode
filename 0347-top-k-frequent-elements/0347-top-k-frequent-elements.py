24b2d9d531d9c603c0b920ed48ad4d50e7c89b50
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-03-14 13:44:57 -0700

    Time: 11 ms (24.19%), Space: 22.8 MB (76.4%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,16 @@
-from bisect import insort
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        fns = []
-        for num, freq in Counter(nums).items():
-            insort(fns, [-freq, num])
-        return [n for f, n in fns[:k:]]
+        freq_nums = collections.defaultdict(set)
+        num_freqs = collections.defaultdict(int)
+        for n in nums:
+            freq_nums[num_freqs[n]].discard(n)
+            num_freqs[n] += 1
+            freq_nums[num_freqs[n]].add(n)
+        res = []
+        for ns in list(freq_nums.values())[::-1]:
+            if k:
+                res.extend(ns)
+            else:
+                return res
+            k -= len(ns)
+        
