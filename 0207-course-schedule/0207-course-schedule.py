2416c93e76c068499a182871fc309d8d00ce10c5
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-03-16 11:22:58 -0700

    Time: 1324 ms (5.1%), Space: 21.5 MB (18.45%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = [[] for _ in range(numCourses)]
+        num_prereqs = [0 for _ in range(numCourses)]
+        for a, b in prerequisites:
+            prereqs[a].append(b)
+            num_prereqs[a] += 1
+        print(prereqs)
+        def rec(remaining: list, completed: set):
+            if not remaining:
+                return True
+            len_completed = len(completed)
+            possible = [course for course in remaining if all(prereq in completed for prereq in prereqs[course])]
+            for course in possible:
+                completed.add(course)
+                remaining.remove(course)
+            if len(completed) == len_completed:
+                return False
+            return rec(remaining, completed)
+        return rec(list(range(numCourses)), set())
