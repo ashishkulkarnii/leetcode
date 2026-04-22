3f60f142b3a40eccdd72deb0b31ee665e17352af
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-04-22 01:13:29 -0700

    Time: 204 ms (14.87%), Space: 19.4 MB (72.62%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        p = list(p)
+        i = 1
+        while i < len(p) - 2:
+            if p[i] == "*" == p[i+2] and p[i-1] == p[i+1]:
+                p.pop(i)
+                p.pop(i)
+            else:
+                i += 1
+        p = "".join(p)
+        return self._isMatch(s, p)
+    def _isMatch(self, s: str, p: str) -> bool:
+        if len(p) > 1 and p[1] == "*":
+            if not s:
+                return self.isMatch(s, p[2:])
+            else:
+                if s[0] == p[0] or p[0] == ".":
+                    return self.isMatch(s, p[2:]) or self.isMatch(s[1:], p[2:]) or self.isMatch(s[1:], p)
+                else:
+                    return self.isMatch(s, p[2:])
+        else:
+            if not (s or p):
+                return True
+            elif s and p and (s[0] == p[0] or p[0] == "."):
+                return self.isMatch(s[1:], p[1:])
+            else:
+                return False
