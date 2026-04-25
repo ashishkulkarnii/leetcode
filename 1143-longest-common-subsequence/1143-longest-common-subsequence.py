6924e2f5542fc8cdc5263d1c9e0530a63ab84914
Ashish Kulkarni <72015808+ashishkulkarnii@users.noreply.github.com>
2026-04-25 12:33:00 -0700

    Time: 391 ms (41.64%), Space: 45.1 MB (34.54%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        i, j = 0, 0
+        m, n = len(text1), len(text2)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[i][j] == max(dp[i-1][j], dp[i][j-1]) + (text1[i] == text2[j])
+        for i in range(m):
+            for j in range(n):
+                if text1[i] == text2[j]:
+                    dp[i][j] = (dp[i-1][j-1] if i > 0 and j > 0 else 0) + 1
+                else:
+                    dp[i][j] = max((dp[i-1][j] if i > 0 else 0), (dp[i][j-1] if j > 0 else 0))
+        return dp[-1][-1]
